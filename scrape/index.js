8e42634b447f7573961a25bb3f04faa0cffa0758
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-02-27 18:26:05 +0100

    more test

--- scrape/index.js
+++ scrape/index.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const params = process.argv.slice(2);
 
-console.log("url1: ", params && params[0]);
-console.log("url2: ", process.env.URL);
+if (params === "WWW.GOOGLE.ES") {
+  console.log("FUNCTIONA");
+} else {
+  console.log("FAIL");
+}
