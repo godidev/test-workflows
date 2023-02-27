8d74b097a6ca85efab1640c17a820bbfd02c27d1
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-02-27 18:12:24 +0100

    add params

--- scrape/index.js
+++ scrape/index.js
@@ -1,2 +1,6 @@
+const params = process.argv.slice(2);
+
 require("dotenv").config();
-console.log(process.env.URL);
+
+console.log("rul1", params && params[0]);
+console.log("url2", process.env.URL);
